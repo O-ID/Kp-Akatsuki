6afbf89be94767a097837406801b1b22f57e427a
nur kholis <nurkholispragaan@gmail.com>
2020-12-30 07:58:25 +0700

    denah

--- resources/views/page/denah.blade.php
+++ resources/views/page/denah.blade.php
@@ -118,26 +118,26 @@
                         <div class="col-md-12">
                             <div class="denah-box">
                                 <img src="{{ asset('assets/img/denah_sekolah.png') }}">
-                                <div class="marker musholla" rel="tooltip" data-html="true" data-title='<b>MUSHOLLA</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker kantin" rel="tooltip" data-html="true" data-title='<b>KANTIN</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker tempat-parkir" rel="tooltip" data-html="true" data-title='<b>TEMPAT PARKIR</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker kantor" rel="tooltip" data-html="true" data-title='<b>KANTOR</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker x-tbsm" rel="tooltip" data-html="true" data-title='<b>X TBSM</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker xi-tbsm" rel="tooltip" data-html="true" data-title='<b>XI TBSM</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker xii-tbsm" rel="tooltip" data-html="true" data-title='<b>XII TBSM</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker ruang-osis" rel="tooltip" data-html="true" data-title='<b>RUANG OSIS</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker ruang-ekskul" rel="tooltip" data-html="true" data-title='<b>RUANG EKSKUL</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker bengkel-motor" rel="tooltip" data-html="true" data-title='<b>BENGKEL MOTOR</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
+                                <div class="marker musholla" rel="tooltip" data-html="true" data-title='<b>MUSHOLLA</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker kantin" rel="tooltip" data-html="true" data-title='<b>KANTIN</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker tempat-parkir" rel="tooltip" data-html="true" data-title='<b>TEMPAT PARKIR</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker kantor" rel="tooltip" data-html="true" data-title='<b>KANTOR</b> </br> <img src="assets/img/denah/kantor.jpg" style="width:160px">'></div>
+                                <div class="marker x-tbsm" rel="tooltip" data-html="true" data-title='<b>X TBSM</b> </br> <img src="assets/img/denah/x tbsm.jpg" style="width:160px">'></div>
+                                <div class="marker xi-tbsm" rel="tooltip" data-html="true" data-title='<b>XI TBSM</b> </br> <img src="assets/img/denah/xi tbsm.jpg" style="width:160px">'></div>
+                                <div class="marker xii-tbsm" rel="tooltip" data-html="true" data-title='<b>XII TBSM</b> </br> <img src="assets/img/denah/xii tbsm.jpg" style="width:160px">'></div>
+                                <div class="marker ruang-osis" rel="tooltip" data-html="true" data-title='<b>RUANG OSIS</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker ruang-ekskul" rel="tooltip" data-html="true" data-title='<b>RUANG EKSKUL</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker bengkel-motor" rel="tooltip" data-html="true" data-title='<b>BENGKEL MOTOR</b> </br> <img src="assets/img/denah/bengkel motor.jpg" style="width:160px">'></div>
+                
+                                <div class="marker halaman-sekolah" rel="tooltip" data-html="true" data-title='<b>HALAMAN SEKOLAH</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
                                 
-                                <div class="marker halaman-sekolah" rel="tooltip" data-html="true" data-title='<b>HALAMAN SEKOLAH</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                
-                                <div class="marker labkom-1" rel="tooltip" data-html="true" data-title='<b>LAB KOMPUTER 1</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker labkom-2" rel="tooltip" data-html="true" data-title='<b>LAB KOMPUTER 2</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker aula" rel="tooltip" data-html="true" data-title='<b>AULA</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker kamar-mandi" rel="tooltip" data-html="true" data-title='<b>KAMR MANDI</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker x-tkj" rel="tooltip" data-html="true" data-title='<b>X TKJ</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker xi-tkj" rel="tooltip" data-html="true" data-title='<b>XI TKJ</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
-                                <div class="marker xii-tkj" rel="tooltip" data-html="true" data-title='<b>XII TKJ</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:100px">'></div>
+                                <div class="marker labkom-1" rel="tooltip" data-html="true" data-title='<b>LAB KOMPUTER 1</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker labkom-2" rel="tooltip" data-html="true" data-title='<b>LAB KOMPUTER 2</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker aula" rel="tooltip" data-html="true" data-title='<b>AULA</b> </br> <img src="assets/img/denah/musholla.jpg" style="width:160px">'></div>
+                                <div class="marker kamar-mandi" rel="tooltip" data-html="true" data-title='<b>KAMR MANDI</b> </br> <img src="assets/img/denah/kamar mandi.jpg" style="width:160px">'></div>
+                                <div class="marker x-tkj" rel="tooltip" data-html="true" data-title='<b>X TKJ</b> </br> <img src="assets/img/denah/x tkj.jpg" style="width:160px">'></div>
+                                <div class="marker xi-tkj" rel="tooltip" data-html="true" data-title='<b>XI TKJ</b> </br> <img src="assets/img/denah/xi tkj.jpg" style="width:160px">'></div>
+                                <div class="marker xii-tkj" rel="tooltip" data-html="true" data-title='<b>XII TKJ</b> </br> <img src="assets/img/denah/xii tkj.jpg" style="width:160px">'></div>
                             </div>
                         </div>
                     </div>
